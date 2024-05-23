59cd055eea0c56712b8debe4a20dbd4a8bd5c627
Andrew Chambers <andrew.chambers.wlv@gmail.com>
2024-05-23 19:23:57 -0400

    Components updated to include server connection, CRUD utilities.

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,14 +2,19 @@ import { useContext } from 'react';
 import FeedbackItem from './FeedbackItem';
 
 import FeedbackContext from '../context/FeedbackContext';
+import Spinner from './shared/Spinner';
 
 const FeedbackList = () => {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>;
   }
-  return (
+
+  return isLoading ? (
+    // <h3>Loading...</h3>
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       {feedback.map(item => (
         <FeedbackItem key={item.id} item={item} />
