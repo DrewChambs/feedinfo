c2a4f4c7a4e0e22f95d3f51b749deb39597e8975
Andrew Chambers <andrew.chambers.wlv@gmail.com>
2024-05-22 22:34:02 -0400

    test commit

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,16 @@
+import Card from '../components/shared/Card';
+import { Link } from 'react-router-dom';
+
+const AboutPage = () => {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About this project</h1>
+        <p>This is a React app to leave feedback for a product</p>
+        <p>Version 1.0.0</p>
+        <Link to="/">Back to Home</Link>
+      </div>
+    </Card>
+  );
+};
+export default AboutPage;
